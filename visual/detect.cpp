9f3a0af8be413e67ff1728b34718618295ef6633
jhauswald <jahausw@umich.edu>
2014-06-27 13:03:00 -0400

    added matching

--- visual/detect.cpp
+++ visual/detect.cpp
@@ -30,21 +30,105 @@ namespace fs = boost::filesystem;
 
 int gpu = 0;
 
-std::vector<std::string> split_string(const std::string& s, std::vector<std::string>& elems)
+vector<string> split_string(const string& s, vector<string>& elems)
 {
-	std::stringstream ss(s);
-	std::string item;
-	while (std::getline(ss, item, ',')) {
+	stringstream ss(s);
+	string item;
+	while (getline(ss, item, ',')) {
 		elems.push_back(item);
 	}
 	return elems;
 }
 
-std::string get_name_from_path(const std::string& class_path)
+string get_name_from_path(const string& class_path)
 {
-	fs::path p = fs::system_complete(class_path);
-	std::string filename = p.filename().string();
+    fs::path p = fs::system_complete(class_path);
+	string filename = p.filename().string();
 	return filename;
+}
+
+vector<KeyPoint> exec_feature(const Mat& img, FeatureDetector* detector)
+{
+	vector<KeyPoint> keypoints;
+	detector->detect(img, keypoints);
+
+	return keypoints;
+}
+
+Mat exec_desc(const Mat& img, DescriptorExtractor* extractor, vector<KeyPoint> keypoints)
+{
+	Mat descriptors;
+
+	extractor->compute(img, keypoints, descriptors);
+
+	descriptors.convertTo(descriptors, CV_32F);
+
+	return descriptors;
+}
+
+void exec_match(po::variables_map& vm)
+{
+    assert(vm.count("database"));
+    assert(vm.count("match"));
+
+    int knn = 1;
+    float threshold = 1;
+
+    // Data
+    Mat testImg;
+    vector<string> trainImgs;
+    Mat testDesc;
+    vector<Mat> trainDesc;
+    vector<DMatch> Matches;
+    vector< vector<DMatch> > knnMatches;
+    vector<int> bestMatches;
+
+    FeatureDetector *detector = new SurfFeatureDetector();
+    DescriptorExtractor *extractor = new SurfDescriptorExtractor();
+    DescriptorMatcher *matcher = new BFMatcher(NORM_L1, false);
+    // DescriptorMatcher *matcher = new FlannBasedMatcher();
+
+    // Generate test desc
+    testImg = imread(vm["match"].as<string>(), CV_LOAD_IMAGE_GRAYSCALE);
+    testDesc = exec_desc(testImg, extractor, exec_feature(testImg, detector));
+
+    // Generate Desc
+    string input_str = vm["database"].as<string>();
+    vector<string> split_names;
+    split_string(input_str, split_names);
+    for(int i = 0; i < split_names.size(); ++i){
+        string& class_path = split_names[i];
+        string class_name = get_name_from_path(class_path);
+        fs::path p = fs::system_complete(class_path);
+        assert(fs::is_directory(p));
+
+        fs::directory_iterator end_iter;
+        for (fs::directory_iterator dir_itr(p); dir_itr != end_iter; ++dir_itr){
+            string img_name(dir_itr->path().string());
+            Mat trainImg = imread(img_name, CV_LOAD_IMAGE_GRAYSCALE);
+            trainDesc.push_back(exec_desc(trainImg, extractor, exec_feature(trainImg, detector)));
+            trainImgs.push_back(img_name);
+            int count = 0;
+            bestMatches.push_back(count);
+        }
+    }
+    
+    // Match
+    matcher->add(trainDesc);
+    matcher->train();
+    matcher->knnMatch(testDesc, knnMatches, knn);
+
+    // Filter results
+    for(vector< vector<DMatch> >::const_iterator it = knnMatches.begin(); it != knnMatches.end(); it++){
+        for(vector<DMatch>::const_iterator it2 = it->begin(); it2 != it->end(); it2++){
+            ++bestMatches[(*it2).imgIdx];
+        }
+    }
+
+    for(int i = 0; i < bestMatches.size(); ++i)
+        cout << "Img: " << trainImgs[i] << " total: " << bestMatches[i] << endl;
+
+    // Find best match
 }
 
 void exec_hog(po::variables_map& vm)
@@ -63,20 +147,20 @@ void exec_hog(po::variables_map& vm)
 #endif
 
 	if(vm.count("stream")){
-		string input_str = vm["stream"].as<std::string>();
+		string input_str = vm["stream"].as<string>();
         vector<string> split_names;
 		split_string(input_str, split_names);
 		for(int i = 0; i < split_names.size(); ++i){
-			std::string& class_path = split_names[i];
-			std::string class_name = get_name_from_path(class_path);
+			string& class_path = split_names[i];
+			string class_name = get_name_from_path(class_path);
 			fs::path p = fs::system_complete(class_path);
 			assert(fs::is_directory(p));
 
 			fs::directory_iterator end_iter;
 			for (fs::directory_iterator dir_itr(p); dir_itr != end_iter; ++dir_itr){
-				std::string img_name(dir_itr->path().string());
+				string img_name(dir_itr->path().string());
                 img = imread(img_name, CV_LOAD_IMAGE_GRAYSCALE);
-                std::vector<cv::Rect> found;
+                vector<cv::Rect> found;
 #ifdef USE_GPU
                 gpu_img.upload(img);
                 hog.detectMultiScale(gpu_img, found);
@@ -90,7 +174,7 @@ void exec_hog(po::variables_map& vm)
     }
     else if(vm.count("image")){
         img = imread(vm["image"].as<string>(), CV_LOAD_IMAGE_GRAYSCALE);
-        std::vector<cv::Rect> found;
+        vector<cv::Rect> found;
 #ifdef USE_GPU
         gpu_img.upload(img);
         hog.detectMultiScale(gpu_img, found);
@@ -111,10 +195,11 @@ po::variables_map parse_opts( int ac, char** av )
 	po::options_description desc("Allowed options");
 	desc.add_options()
 		("help,h", "Produce help message")
+		("match,m", po::value<string>(), "Test Image for match in database")
+		("database,d", po::value<string>(), "Database to test for match")
+		("image,e", po::value<string>(), "Input Image")
+		("stream,s", po::value<string>(), "Folders with image stream separated by a comma.")
 		("walk,k", po::value<bool>()->default_value(false), "Stream images to a HOG person detector")
-		("image,e", po::value<std::string>(), "Input Image")
-		("stream,s", po::value<std::string>(),
-		 "Folders with image stream separated by a comma. If loading features/descriptors, pass folders directly")
 
 		("gpu,u", po::value<bool>()->default_value(false), "Use GPU? Only for specific algorithms") 
 
@@ -126,7 +211,7 @@ po::variables_map parse_opts( int ac, char** av )
 	po::notify(vm);    
 
 	if (vm.count("help")) {
-		std::cout << desc << "\n";
+		cout << desc << "\n";
 		return vm;
 	}
 	return vm;
@@ -138,6 +223,8 @@ int main( int argc, char** argv )
     
 	if(vm["walk"].as<bool>()){
 		exec_hog(vm);
+    }else if(vm.count("match")){
+        exec_match(vm);
 	}else{
 		cout << "For help: " << argv[0] << " --help" << endl;
 	}
