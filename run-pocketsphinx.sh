1d33159d1779f8e772cf04cac051c7f18b023ef0
jhauswald <jahausw@umich.edu>
2014-06-30 15:51:26 -0400

    STOP hardcoding paths into scripts

--- run-pocketsphinx.sh
+++ run-pocketsphinx.sh
@@ -20,6 +20,7 @@ pocketsphinx_continuous \
     -infile $w \
     -lm models/lm_giga_64k_nvp_3gram.lm.DMP \
     -hmm models/voxforge_en_sphinx.cd_cont_5000 \
-    -dict models/cmu07a.dic
+    -dict models/cmu07a.dic \
+    -logfn /dev/null
 
 done
--- run-sphinx4-server.sh
+++ run-sphinx4-server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export MODELS_PATH="/home/kmcatee/umvoice/models/"
-export CONF_FILE="/home/kmcatee/umvoice/sphinx_batch_conf.xml"
+export MODELS_PATH="`pwd`models/"
+export CONF_FILE="`pwd`/sphinx_batch_conf.xml"
 
 #java -Djava.library.path=./lib -cp .:./lib/servlet-api.jar:./lib/jetty-all.jar:lib/sphinx4-core-1.0-SNAPSHOT.jar Sphinx4Server
 
