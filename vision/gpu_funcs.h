b15adda92a71aca28c0cb67edc72aadf87a2a4b0
jhauswald <jahausw@umich.edu>
2014-07-12 12:15:13 -0400

    std

--- vision/gpu_funcs.h
+++
@@ -1,8 +0,0 @@
-#ifndef GPU_FUNCS_H
-#define GPU_FUNCS_H
-
-std::vector<KeyPoint> exec_feature_gpu(const Mat& img, const std::string detector_str);
-
-Mat exec_descriptor_gpu(const Mat& img_name, const std::string extractor_str, std::vector<KeyPoint> keypoints);
-
-#endif
