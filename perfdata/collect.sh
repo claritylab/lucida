a913dec639fe7607bfce32a5af7afc1eec787bd3
root <root@clarityacc.eecs.umich.edu>
2014-07-17 09:15:18 -0400

    new

--- perfdata/collect.sh
+++ perfdata/collect.sh
@@ -2,6 +2,16 @@
 
 RESDIR=`pwd`/logs
 mkdir -p $RESDIR
+RESDIR1=`pwd`/logs1
+mkdir -p $RESDIR
+RESDIR2=`pwd`/logs2
+mkdir -p $RESDIR2
+
+stats=instructions,cycles,stalled-cycles-frontend,cache-misses,cache-references
+#stats=instructions,cycles,branches,branch-misses,cache-misses,cache-references
+stats1=L1-dcache-load-misses,L1-dcache-store-misses,L1-icache-load-misses,LLC-stores,LLC-loads,stalled-cycles-frontend 
+#stalls,llc_refs,llc_miss,stalls_ldm_pending,arith
+stats2=r5301a2,r534f2e,r53412e,r65306a3,r530114
 
 cd bins ;
 for kernel in regex crfsuite pocketsphinx porter
@@ -9,8 +19,26 @@ do
 	for i in 1 2 3 4 5 6 7 8 9 0
 	do
 		echo $kernel run: $i
-		perf stat -e instructions,cycles,stalled-cycles-frontend,cache-misses,cache-references -o ${kernel}_log${i}.txt ./${kernel}_script.sh
-		# perf stat -e instructions,cycles,branches,branch-misses,cache-misses,cache-references -o ${kernel}_log${i}.txt ./${kernel}_script.sh
+		perf stat -e $stats -o ${kernel}_log${i}.txt ./${kernel}_script.sh
 		mv ${kernel}_log${i}.txt $RESDIR
+		perf stat -e $stats1 -o ${kernel}_log${i}.txt ./${kernel}_script.sh
+		mv ${kernel}_log${i}.txt $RESDIR1
+		perf stat -e $stats2 -o ${kernel}_log${i}.txt ./${kernel}_script.sh
+		mv ${kernel}_log${i}.txt $RESDIR2
 	done
 done
+
+cd ../../kernels/dnn/example-setup/recognition > /dev/null;
+
+kernel=dnn
+for i in 1 2 3 4 5 6 7 8 9 0
+do
+	echo $kernel run: $i
+	perf stat -e $stats -o ${kernel}_log${i}.txt ./30_nn-recognition-hybrid.sh
+	mv ${kernel}_log${i}.txt $RESDIR
+	perf stat -e $stats1 -o ${kernel}_log${i}.txt ./30_nn-recognition-hybrid.sh
+	mv ${kernel}_log${i}.txt $RESDIR1
+	perf stat -e $stats2 -o ${kernel}_log${i}.txt ./30_nn-recognition-hybrid.sh
+	mv ${kernel}_log${i}.txt $RESDIR2
+done
+
--- perfdata/parse.sh
+++ perfdata/parse.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Script to parse output of perf tool
-plat=pc
+plat=vinipc
 
 function get_data {
 	for i in 1 2 3 4 5 6 7 8 9 0
@@ -18,10 +18,20 @@ function get_rate {
 	done
 }
 
+function sum_data {
+	for i in 1 2 3 4 5 6 7 8 9 0
+	do
+		echo -n "$1,$2,$plat,$i,"
+        v1=$( cat ${1}_log${i}.txt | grep $3 | awk {'print $1'} | sed 's/,//g')
+        v2=$( cat ${1}_log${i}.txt | grep $4 | awk {'print $1'} | sed 's/,//g')
+        echo $(awk "BEGIN {printf \"%d\",$(( $v1+$v2 ))}")
+	done
+}
+
+echo "Benchmark, Stat, Platform, Run#, Value"
 cd logs > /dev/null;
 sed -i -e 's/,//g' *
-echo "Benchmark, Stat, Platform, Run#, Value"
-for kernel in regex crfsuite pocketsphinx porter
+for kernel in regex crfsuite pocketsphinx porter dnn
 do
 	get_data $kernel "instructions" instructions
 	get_data $kernel "cycles" cycles
@@ -30,8 +40,33 @@ do
 	get_data $kernel "D-Cache-Accesses" cache-references
 	get_data $kernel "D-Cache Misses" cache-misses
 	get_rate $kernel "D-Cache Miss rate" refs
-	# get_data $kernel "Branches" branches
-	# get_data $kernel "Branch Misses" branch-misses
+#	get_data $kernel "Branches" branches
+#	get_data $kernel "Branch Misses" branch-misses
 	get_data $kernel "Runtime" seconds
+done
+
+cd logs1 > /dev/null;
+sed -i -e 's/,//g' *
+for kernel in regex crfsuite pocketsphinx porter dnn
+do
+	get_data $kernel "L3 Loads" LLC-loads
+	get_data $kernel "L3 Stores" LLC-stores
+	sum_data $kernel "LLC-Accesses" LLC-loads LLC-stores
+	get_data $kernel "L1 I-Cache Load Misses" L1-icache-load-misses
+	get_data $kernel "L1 D-Cache Load Misses" L1-dcache-load-misses
+	get_data $kernel "L1 D-Cache Store Misses" L1-dcache-store-misses
+	sum_data $kernel "L1-D-Cache-Accesses" L1-dcache-load-misses L1-dcache-store-misses
+	get_data $kernel "Stalled cycles" stalled
+done
+
+cd logs2 > /dev/null;
+sed -i -e 's/,//g' *
+for kernel in regex crfsuite pocketsphinx porter dnn
+do
+	get_data $kernel "Resource-stalls" 5301a2 
+	get_data $kernel "Stalls-ldm" 65306a3
+	get_data $kernel "LLC-refs" 534f2e 
+	get_data $kernel "LLC-misses" 53412e 
+	get_data $kernel "Arith-units" r530114
 done
 
