98328ffd5d9ef9e3a22bc417f95b6f92d00259b0
jhauswald <jahausw@umich.edu>
2015-01-06 17:43:48 -0500

    rm crf and HMM

--- sirius-suite/crf/crfsuite-0.12/go.sh
+++
@@ -1,5 +0,0 @@
-make
-make install
-cd example/ ;
-crfsuite tag -qt -m model.model test.crfsuite.txt
-cd - ;
--- sirius-suite/crf/crfsuite-0.12/swig/python/prepare.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-ln -s ../crfsuite.cpp
-ln -s ../export.i
-
-if [ "$1" = "--swig" ];
-then
-    swig -c++ -python -I../../include -o export_wrap.cpp export.i
-fi
