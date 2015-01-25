20f8509885e79232918cc5415bed84536854d76d
jahausw <jahausw@umich.edu>
2015-01-25 15:50:36 -0500

    speech input, record and image html updates

--- sirius-web/web-sirius.py
+++ sirius-web/web-sirius.py
@@ -5,12 +5,17 @@ import urllib, urllib2, httplib
 from datetime import datetime
 from httplib import BadStatusLine
 import multiprocessing
-from record import *
 from flask import Flask, request, render_template, redirect, url_for
+from werkzeug import secure_filename
+from OpenSSL import SSL
+
+UPLOAD_FOLDER='input-log/'
+ALLOWED_EXTENSIONS=set(['wav'])
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-servers = ['localhost']
+servers = ['141.212.106.240']
 QA  = servers[0]
 ASR = servers[0]
 VIS = servers[0]
@@ -40,6 +45,9 @@ def req_vis(image, return_dict=None):
     return result
 
 def req_asr(speech, return_dict=None):
+    # # convert to 16KHz
+    # cmd = 'ffmpeg -y -i ' + filename + ' -acodec pcm_s16le -ac 1 -ar 16000 input-log/tmp.wav'
+    # shcmd(cmd)
     cmd = 'wget -q -U "Mozilla/5.0" --post-file ' + str(speech)
     cmd += ' --header "Content-Type: audio/vnd.wave; rate=16000" -O - '
     cmd += make_server(ASR, ports[1])
@@ -50,7 +58,6 @@ def req_asr(speech, return_dict=None):
     return result
 
 def req_qa(text, return_dict=None):
-    print text
     words = text.split()
     query = ''
     for w in words:
@@ -66,10 +73,15 @@ def req_qa(text, return_dict=None):
     except BadStatusLine:
         answer = ''
 
+    print text
+    print answer
     return answer
 
 def make_server(ip, port):
     return str('http://' + ip + ':' + str(port))
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 class RegistrationForm(Form):
     qs = TextField('Type something', [validators.Length(min=2, max=200)])
@@ -79,37 +91,35 @@ class RegistrationForm(Form):
 def image():
     form = RegistrationForm(request.form)
     if request.method == 'POST':
-        # record + timestamp
-        t = datetime.now()
-        # speech = log + 'speech-' + str(t.hour) + str(t.minute) + str(t.second) + '.wav'
-        # record_to_file(speech)
-        speech = 'demo_img.wav'
-        # get image + timestamp
-        if re.search('.jpg', form.in_img.data) and os.path.isfile(form.in_img.data):
-            img = form.in_img.data
-        else:
+        f = request.files['file']
+        if f and allowed_file(f.filename):
+            filename = secure_filename(f.filename)
+            f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+            speech = 'input-log/' + filename
+            t = datetime.now()
             img = log + 'image-' + str(t.hour) + str(t.minute) + str(t.second) + '.jpg'
             urllib.urlretrieve(form.in_img.data, img)
 
-        # launch vis, speech in parallel
-        manager = multiprocessing.Manager()
-        return_dict = manager.dict()
+            # launch vis, speech in parallel
+            manager = multiprocessing.Manager()
+            return_dict = manager.dict()
 
-        fns = [req_vis, req_asr]
-        data = [img, speech]
-        proc = []
-        for idx,fn in enumerate(fns):
-            p = multiprocessing.Process(target=fn, args=(data[idx], return_dict))
-            p.start()
-            proc.append(p)
-        for p in proc:
-            p.join()
+            fns = [req_vis, req_asr]
+            data = [img, speech]
+            proc = []
+            for idx,fn in enumerate(fns):
+                p = multiprocessing.Process(target=fn, args=(data[idx], return_dict))
+                p.start()
+                proc.append(p)
+            for p in proc:
+                p.join()
 
-        text = re.sub('this', return_dict[img], return_dict[speech])
-        answer = req_qa(text)
-        q = 'Question: %s' % text
-        a = 'Answer: %s' % answer
-        return render_template('image.html', form=form, in_img=form.in_img.data, question=q, answer=a)
+            text = re.sub('this', return_dict[img], return_dict[speech])
+            answer = req_qa(text)
+            q = 'Question: %s' % text
+            a = 'Answer: %s' % answer
+            return render_template('image.html', form=form, in_img=form.in_img.data, question=q, answer=a)
     else:
         return render_template('image.html', form=form)
 
@@ -117,17 +127,19 @@ def image():
 def record():
     form = RegistrationForm(request.form)
     if request.method == 'POST':
-        t = datetime.now()
-        filename = log + 'speech-' + str(t.hour) + str(t.minute) + str(t.second) + '.wav'
-        record_to_file(filename)
-        cmd = 'afplay ' + filename
-        shcmd(cmd)
-        text = req_asr(filename)
-        answer = req_qa(text)
+        f = request.files['file']
+        if f and allowed_file(f.filename):
+            filename = secure_filename(f.filename)
+            f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
-        line1 = 'Question: %s' % text
-        line2 = 'Answer: %s' % answer
-        return render_template('record.html', form=form, reply_line1=line1, reply_line2=line2)
+            filename = 'input-log/' + filename
+            text = req_asr(filename)
+            # text = req_asr('input-log/tmp.wav')
+            answer = req_qa(text)
+
+            line1 = 'Question: %s' % text
+            line2 = 'Answer: %s' % answer
+            return render_template('record.html', form=form, reply_line1=line1, reply_line2=line2)
     else:
         return render_template('record.html', form=form)
 
@@ -155,4 +167,10 @@ def index():
 if __name__ == "__main__":
     cmd = 'mkdir -p ' + log
     shcmd(cmd)
-    app.run(host='0.0.0.0', port=8000, debug=True)
+
+    pkey = os.getcwd() + '/server.key'
+    cert = os.getcwd() + '/server.crt'
+    context = SSL.Context(SSL.SSLv3_METHOD)
+    context.use_privatekey_file(pkey)
+    context.use_certificate_file(os.getcwd() + '/server.crt')
+    app.run(host='141.212.106.240', port=8000, debug=True, ssl_context=(cert, pkey) )
