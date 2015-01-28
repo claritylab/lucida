3125416445030db920cff7705d9a8b3c328581c3
jahausw <jahausw@umich.edu>
2015-01-27 19:56:33 -0500

    backup shit demo

---
+++ sirius-web/demo/demo-asr.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+QA="http://141.212.106.240:8080"
+ASR="http://141.212.106.240:8081"
+
+read -p "Press [Enter] key to start speaking..."
+ 
+echo "Recording... Press Ctrl+C to Stop."
+sox -d speech.wav 1> /dev/null 2> /dev/null
+echo "(1) Converting your audio to suitable format for speech processing..."
+ffmpeg -i -y speech.wav -acodec pcm_s16le -ac 1 -ar 16000 speech2.wav  1>/dev/null 2>/dev/null
+
+echo "(2) Just to make sure, you said..."
+play speech2.wav 1> /dev/null 2>/dev/null
+
+echo "(3) OK, now sending the recorded speech to server..."
+
+# resp=`wget -q -U "Mozilla/5.0" --post-file speech2.wav --header "Content-Type: audio/vnd.wave; rate=16000" -O - $ASR`
+resp=`wget -q -U "Mozilla/5.0" --post-file where.is.the.louvre.museum.located.wav --header "Content-Type: audio/vnd.wave; rate=16000" -O - $ASR`
+
+echo "Speech-to-text:" $resp
+
+query=`echo $resp | cut -d: -f2`
+query="$query $resp_img"
+
+echo "Query: " $query
+
+query2=$(echo -n $query | perl -pe's/([^-_.~A-Za-z0-9])/sprintf("%%%02X", ord($1))/seg');
+
+resp2=`curl --request GET "$QA?query=$query2" 2>/dev/null`
+
+echo "QA response:" $resp2
+
+say $resp2
