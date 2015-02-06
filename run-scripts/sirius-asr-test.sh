b55a97008de19523954e9144fe897fa79a8d9055
Yiping Kang <ypkang@umich.edu>
2015-02-06 00:19:11 -0500

    Kaldi plugged in

--- run-scripts/sirius-asr-test.sh
+++ run-scripts/sirius-asr-test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 IFS=$'\n'; 
-for line in `cat sirius-question.txt`;
+for wav_file in /home/ypkang/input-log/*.wav;
 do
     echo "(1) Your voice search (text) is:"
     echo "$line"      
@@ -9,8 +9,11 @@ do
     filename=${line// /.}
     filename=`echo $filename | tr '[:upper:]' '[:lower:]'`
 
+    echo $wav_file
     echo "(2) Sending request to server..."
-    resp=`wget -q -U "Mozilla/5.0" --post-file ../inputs/questions/$filename.wav --header "Content-Type: audio/vnd.wave; rate=16000" -O - "localhost:8081/" `
+    resp=`wget -q -U "Mozilla/5.0" --post-file $wav_file \
+                                    --header "Content-Type: audio/vnd.wave; rate=16000" \
+                                    -O - "localhost:8081/" `
     
     echo "Resp: "$resp | cut -d: -f2
 
