8ffc292f0e0c9e58774a8ae5c445096de7ba3f58
yqzhang <zhangyunqi.cs@gmail.com>
2015-02-06 04:27:24 -0500

    [Sirius] Add license for sirius-application

--- sirius-application/image-matching/kp_protobuf.cpp
+++ sirius-application/image-matching/kp_protobuf.cpp
@@ -1,3 +1,20 @@
+/*
+ *  Copyright (c) 2015, University of Michigan.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+/**
+ * TODO:
+ *
+ * @author: Johann Hauswald
+ * @contact: jahausw@umich.edu
+ */
+
 #include "kp_protobuf.h"
 
 using namespace cv;
