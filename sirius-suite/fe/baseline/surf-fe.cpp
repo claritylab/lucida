524f3cd0d3373e6f74a39d0e41515e3b1b2526ac
yqzhang <zhangyunqi.cs@gmail.com>
2015-02-06 04:13:36 -0500

    [Sirius] Add license for sirius-suite

--- sirius-suite/fe/baseline/surf-fe.cpp
+++ sirius-suite/fe/baseline/surf-fe.cpp
@@ -1,6 +1,18 @@
-/* Johann Hauswald
- * jahausw@umich.edu
- * 2014
+/*
+ *  Copyright (c) 2015, University of Michigan.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+/**
+ * TODO:
+ *
+ * @author: Johann Hauswald
+ * @contact: jahausw@umich.edu
  */
 
 #include <assert.h>
