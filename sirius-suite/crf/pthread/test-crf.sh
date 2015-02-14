811cc0214754031631e8c386befea058d2bac531
jahausw <jahausw@umich.edu>
2015-02-14 15:57:28 -0500

    tw

--- sirius-suite/crf/pthread/test-crf.sh
+++ sirius-suite/crf/pthread/test-crf.sh
@@ -4,9 +4,9 @@
 
 ./crfsuite tag -qt -m ../input/model.model ../input/test.crfsuite.txt
 
-diff ../input/crf.pthread ../input/crf.baseline > /dev/null
+out=$( diff ../input/crf.pthread ../input/crf.baseline > /dev/null )
 
-if [ $$? -eq 1 ] 
+if [ $? -eq 1 ] 
 then 
     echo "CRF-PTHREAD test failed"
 fi
