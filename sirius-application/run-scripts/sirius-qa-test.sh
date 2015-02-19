a824f3440dfb7a40ad4be57543fcf47920b5fceb
jahausw <jahausw@umich.edu>
2015-02-19 11:33:49 -0500

    tw

--- sirius-application/run-scripts/sirius-qa-test.sh
+++ sirius-application/run-scripts/sirius-qa-test.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 
 QA="http://localhost:8080"
+query=$1
 
-IFS=$'\n'; 
-for line in `cat sirius-question.txt`;
-do
-        echo "(1) Your query text is:"
-        echo "$line"      
-        
-        query=$(echo -n $line | perl -pe's/([^-_.~A-Za-z0-9])/sprintf("%%%02X", ord($1))/seg');
+echo "(1) Your query text is:"
+echo "$query"
 
-        echo "(2) Sending request to server..."
-        curl --request GET "$QA?query=$query"
-             
-        echo "***********************************************"
-done
+query=$(echo -n $query | perl -pe's/([^-_.~A-Za-z0-9])/sprintf("%%%02X", ord($1))/seg');
 
+echo "(2) Sending request to server..."
+curl --request GET "$QA?query=$query"
+
+echo "***********************************************"
