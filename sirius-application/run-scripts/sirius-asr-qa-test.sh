208848916c2f2e530ab8c9d79620d19158d9e30e
jahausw <jahausw@umich.edu>
2015-02-19 12:05:14 -0500

    tw asr+qa

--- sirius-application/run-scripts/sirius-asr-qa-test.sh
+++ sirius-application/run-scripts/sirius-asr-qa-test.sh
@@ -1,28 +1,24 @@
 #!/bin/bash
 
-ASR="http://141.212.106.240:8081/"
-QA="http://141.212.106.240:8080/"
+ASR="http://localhost:8081/"
+QA="http://localhost:8080/"
+query=$1
 
-IFS=$'\n'; 
-for line in `cat sirius-questions.txt`;
-do
-    echo "Your voice search (text) is:"
-    echo "$line"      
+echo "Your voice search (text) is:"
+echo "$query"
 
-    filename=${line// /.}
-    filename=`echo $filename | tr '[:upper:]' '[:lower:]'`
+filename=${query// /.}
+filename=`echo $filename | tr '[:upper:]' '[:lower:]'`
 
-    echo "Sending request to ASR server..."
-    resp=`wget -q -U "Mozilla/5.0" --post-file ../inputs/questions/$filename.wav \
-                                   --header "Content-Type: audio/vnd.wave; rate=16000" \
-                                   -O - "$ASR" `
-    
-    query=`echo $resp | cut -d: -f2`  
-    query2=$(echo -n $query | perl -pe's/([^-_.~A-Za-z0-9])/sprintf("%%%02X", ord($1))/seg');
+echo "Sending request to ASR server..."
+resp=`wget -q -U "Mozilla/5.0" --post-file ../inputs/questions/$filename.wav \
+    --header "Content-Type: audio/vnd.wave; rate=16000" \
+    -O - "$ASR" `
 
-    echo "Sending request to QA server..."
-    curl --request GET "$QA?query=$query2"
+query=`echo $resp | cut -d: -f2`  
+query2=$(echo -n $query | perl -pe's/([^-_.~A-Za-z0-9])/sprintf("%%%02X", ord($1))/seg');
 
-    echo "***********************************************"
-done
+echo "Sending request to QA server..."
+curl --request GET "$QA?query=$query2"
 
+echo "***********************************************"
