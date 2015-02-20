125e3d8bf3a3dbdc6052cfbf11321049fc4d6675
Yiping Kang <ypkang@umich.edu>
2015-02-19 23:34:25 -0500

    Kaldi-ASR: model loading happens at first, server always on

--- sirius-application/run-scripts/sirius-asr-test.sh
+++ sirius-application/run-scripts/sirius-asr-test.sh
@@ -2,14 +2,14 @@
 
 wav_file=$1
 
-ASR="localhost:8081/"
+ASR="localhost:8091/"
 
 echo "Your audio file is:"
 echo $wav_file
 echo "Sending request to server..."
 resp=`wget -q -U "Mozilla/5.0" --post-file $wav_file \
     --header "Content-Type: audio/vnd.wave; rate=16000" \
-    -O - $ASR" `
+    -O - $ASR `
 
 echo "Resp: "$resp | cut -d: -f2
 
