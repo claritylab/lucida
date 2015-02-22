5bc246c29bb668b6616a1c00c6e5355cd557f2e8
yqzhang <zhangyunqi.cs@gmail.com>
2015-02-22 00:08:45 -0500

    EUID does not work on travis-ci

--- sirius-application/get-dependencies.sh
+++ sirius-application/get-dependencies.sh
@@ -2,19 +2,13 @@
 
 # Install (some) dependencies
 
-if [ "$EUID" -ne 0 ]
-then
-  echo >&2 "$0: [ERROR] Sudo access required. Aborting."
-  exit 1
-fi
-
-apt-get install \
+sudo apt-get install \
   default-jdk ant automake autoconf libtool bison libboost-all-dev ffmpeg
 
 # Get tessaract text recognition
-apt-get install \
+sudo apt-get install \
   tesseract-ocr tesseract-ocr-eng libtesseract-dev libleptonica-dev
 
 # Get ATLAS library for Kaldi
-apt-get install \
+sudo apt-get install \
   libatlas3-base  
--- sirius-application/get-opencv.sh
+++ sirius-application/get-opencv.sh
@@ -14,14 +14,8 @@ hash unzip 2>/dev/null || {
   exit 1
 }
 
-if [ "$EUID" -ne 0 ]
-then
-  echo >&2 "$0: [ERROR] Sudo access required. Aborting."
-  exit 1
-fi
-
 # Get correct opencv
-apt-get install \
+sudo apt-get install \
   build-essential checkinstall git cmake libfaac-dev libjack-jackd2-dev \
   libmp3lame-dev libopencore-amrnb-dev libopencore-amrwb-dev libsdl1.2-dev \
   libtheora-dev libva-dev libvdpau-dev libvorbis-dev libx11-dev libxfixes-dev \
