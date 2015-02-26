f6a14fc724ec4eb282840c8d39166ce32feee7e6
jahausw <jahausw@umich.edu>
2015-02-25 20:42:14 -0500

    a new vini unfuck: remove source package as a zip

--- sirius-application/run-scripts/start-qa-server.sh
+++ sirius-application/run-scripts/start-qa-server.sh
@@ -34,8 +34,6 @@ cd .. ;
 export CLASSPATH=bin:lib/ml/maxent.jar:lib/ml/minorthird.jar:lib/nlp/jwnl.jar:lib/nlp/lingpipe.jar:lib/nlp/opennlp-tools.jar:lib/nlp/plingstemmer.jar:lib/nlp/snowball.jar:lib/nlp/stanford-ner.jar:lib/nlp/stanford-parser.jar:lib/nlp/stanford-postagger.jar:lib/qa/javelin.jar:lib/search/bing-search-java-sdk.jar:lib/search/googleapi.jar:lib/search/indri.jar:lib/search/yahoosearch.jar:lib/util/commons-logging.jar:lib/util/gson.jar:lib/util/htmlparser.jar:lib/util/log4j.jar:lib/util/trove.jar:lib/util/servlet-api.jar:lib/util/jetty-all.jar:lib/util/commons-codec-1.9.jar
 
 export INDRI_INDEX=`pwd`/wiki_indri_index/
-# FIXME
-# I don't know how. OE complains with threads < 8 :(
 export THREADS=8
 
 cd question-answer ;
