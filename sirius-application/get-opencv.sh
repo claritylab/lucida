92a2842c4cfaa3ef059c36325dea55f4a001346c
Uwe Koloska <kolewu@koloro.de>
2015-03-24 00:27:17 +0100

    streamline get-opencv.sh

--- sirius-application/get-opencv.sh
+++ sirius-application/get-opencv.sh
@@ -19,12 +19,12 @@ hash unzip 2>/dev/null || {
 NUM_THREADS=4
 ver=2.4.9
 
-git clone https://github.com/Itseez/opencv.git;
-mv opencv opencv-$ver
-cd opencv-$ver;
-git checkout $ver;
-mkdir build;
-cd build;
-cmake ..;
+git clone https://github.com/Itseez/opencv.git opencv-$ver
+cd opencv-$ver
+git checkout $ver
+mkdir build
+cd build
+cmake ..
 make -j$NUM_THREADS
 make -j$NUM_THREADS install
+
