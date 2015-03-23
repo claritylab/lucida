aeedc930f018a81018ba616beb22cb2c64fc37ff
Uwe Koloska <kolewu@koloro.de>
2015-03-24 00:22:09 +0100

    get the dependencies right and in one place

--- sirius-application/get-opencv.sh
+++ sirius-application/get-opencv.sh
@@ -19,13 +19,6 @@ hash unzip 2>/dev/null || {
 NUM_THREADS=4
 ver=2.4.9
 
-# Get correct opencv
-apt-get install -y \
-  build-essential checkinstall git cmake libfaac-dev libjack-jackd2-dev \
-  libmp3lame-dev libopencore-amrnb-dev libopencore-amrwb-dev libsdl1.2-dev \
-  libtheora-dev libva-dev libvdpau-dev libvorbis-dev libx11-dev libxfixes-dev \
-  libxvidcore-dev texi2html yasm zlib1g-dev
-
 git clone https://github.com/Itseez/opencv.git;
 mv opencv opencv-$ver
 cd opencv-$ver;
