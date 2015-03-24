8ac038d6f61f85a1f42f3d36bc57d6fcdfccd065
Uwe Koloska <kolewu@koloro.de>
2015-03-24 01:19:35 +0100

    the vagrant specific files for the current state

--- sirius-vm/scripts/install.sh
+++ sirius-vm/scripts/install.sh
@@ -1,16 +1,9 @@
 #!/usr/bin/env bash
 
-# Add additional repositories (ffmpeg)
-add-apt-repository ppa:jon-severinsson/ffmpeg
-
-# Enable multiverse sources (libfaac-dev)
-sed -i "/^# deb.*multiverse/ s/^# //" /etc/apt/sources.list
-# possible better option for newer ubuntu releases:
-#apt-add-repository multiverse
-
-# Update sources and install basics
-apt-get update
-apt-get -y install vim git zip unzip dos2unix
+# install some helper for working inside the vm
+sudo apt-get update
+sudo apt-get -y install \
+     vim dos2unix
 
 # Change to the application directory
 cd /home/sirius/sirius-application
@@ -20,6 +13,6 @@ cd /home/sirius/sirius-application
 find . -iname '*.sh' -print0 | xargs -0 dos2unix
 
 sudo sh ./get-dependencies.sh 
-#sh ./get-kaldi.sh
-sudo sh ./get-opencv.sh
-#sudo sh ./compile-sirius-servers.sh
+./get-kaldi.sh
+./get-opencv.sh
+THREADS=1 ./compile-sirius-servers.sh
