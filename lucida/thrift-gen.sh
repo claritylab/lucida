66810281208ef58ae0b950d3d37c0fd223c6eb8f
Johann Hauswald <johann@clinc.ai>
2016-03-19 18:25:24 -0400

    [build] - cleaner build reflected in Dockerfile - removed old thrift definitions not used
    Conflicts:
    	lucida/commandcenter/tmuxdemo.sh

--- lucida/thrift-gen.sh
+++ lucida/thrift-gen.sh
@@ -1,12 +1,19 @@
 # thrift generator script
-thrift --gen cpp service.thrift
 
-cd command-center;
+# services
+cd $LUCIDAROOT/questionanswering/lucida;
+thrift --gen java --gen cpp qaservice.thrift
+
+cd $LUCIDAROOT/commandcenter;
 thrift --gen cpp --gen java --gen js:node commandcenter.thrift
 thrift --gen cpp --gen js:node filetransfer_svc.thrift
-thrift --gen cpp ../qa/lucida/qaservice.thrift
-thrift --gen cpp ../asr/lucida/kaldi.thrift
-thrift --gen cpp ../imm/lucida/service.thrift
+thrift --gen cpp $LUCIDAROOT/questionanswering/lucida/qaservice.thrift
+thrift --gen cpp $LUCIDAROOT/speechrecognition/lucida/kaldi.thrift
+thrift --gen cpp $LUCIDAROOT/imagematching/lucida/service.thrift
 
-cd -; cd qa/lucida;
-thrift --gen java --gen cpp qaservice.thrift
+cd $LUCIDAROOT/commandcenter-py
+thrift --gen py $LUCIDAROOT/commandcenter/commandcenter.thrift
+thrift -r --gen py $LUCIDAROOT/questionanswering/lucida/qaservice.thrift
+thrift -r --gen py $LUCIDAROOT/speechrecognition/lucida/kaldi.thrift
+thrift -r --gen py $LUCIDAROOT/imagematching/lucida/service.thrift
+thrift -r --gen py $LUCIDAROOT/djinntonic/lucidaservice.thrift
