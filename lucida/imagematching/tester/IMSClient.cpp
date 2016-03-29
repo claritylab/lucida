02839a03076b1886fa91ac55eed8734a1bd711ac
yunshengb <yba@umich.edu>
2016-03-28 23:12:29 -0400

    Clean up IMM

--- lucida/imagematching/tester/IMSClient.cpp
+++ lucida/imagematching/tester/IMSClient.cpp
@@ -1,6 +1,5 @@
 /*
- *
- *
+ * Implementation for the Image Matching service testing client.
  */
 
 #include <iostream>
@@ -20,38 +19,33 @@ using namespace apache::thrift::protocol;
 using namespace apache::thrift::transport;
 
 int main(int argc, char** argv) {
-	int port = 9082;
+	// shared_ptr the port number and  image path.
+	int port = 8081;
 	string imagePath;
-	
-	if (argv[1]) {
-		port = atoi(argv[1]);
-	} else {
-		cout << "Using default port for imm..." << endl;
-	} 
-
-	if(argv[2]) {
-		imagePath = argv[2];
-	} else {
+	if (argc != 3) {
 		cerr << "Usage: ./client <imm_port> <path_to_image>" << endl;
-		return -1;
+		return 1;
 	}
-
+	port = atoi(argv[1]);
+	imagePath = argv[2];
+	// Initialize Thrift objects.
 	boost::shared_ptr<TTransport> socket(new TSocket("localhost", port));
 	boost::shared_ptr<TTransport> transport(new TBufferedTransport(socket));
 	boost::shared_ptr<TProtocol> protocol(new TBinaryProtocol(transport));
 	LucidaServiceClient client(protocol);
-	
-	struct timeval tv1, tv2;
-	try{
+	try {
+		// Open the image.
 		ifstream fin(imagePath.c_str(), ios::binary);
 		ostringstream ostrm;
 		ostrm << fin.rdbuf();
 		string image(ostrm.str());
-		if (!fin) cerr << "Could not open the file!" << endl;
-
+		if (!fin) {
+			cerr << "Could not open the file!" << endl;
+			return 1;
+		}
+		// Prepare to interact with the IMM server.
 		transport->open();
 		string response;
-
 		// Create a QueryInput and a QuerySpec.
 		QueryInput query_input;
 		query_input.type = "image";
@@ -59,13 +53,14 @@ int main(int argc, char** argv) {
 		QuerySpec query_spec;
 		query_spec.content.push_back(query_input);
 		string LUCID = "Johann";
-
+		// Invoke the infer service.
 		client.infer(response, LUCID, query_spec);
-		cout << "client sent the image successfully..." << endl;
+		// Print the result.
+		cout << "Client sent the image successfully..." << endl;
 		cout << response << endl;
-		
+		// Close the connection.
 		transport->close();
-	}catch(TException &tx){
+	} catch(TException &tx){
 		cout << "ERROR: " << tx.what() << endl;
 	}
 
