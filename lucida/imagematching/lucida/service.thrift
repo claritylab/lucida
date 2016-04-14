971741d905306f98b062d3dc0da9812506737307
yunshengb <yba@umich.edu>
2016-04-14 10:07:22 -0400

    Clean up

--- lucida/imagematching/lucida/service.thrift
+++
@@ -1,6 +0,0 @@
-# The ImageMatchingService is running on the server to accept image queries
-# and return the name that best matches in the pre-trained database  
-service ImageMatchingService {
-  string send_request(1:binary img_query);
-  void ping()
-}
--- lucida/speechrecognition/lucida/kaldi.thrift
+++
@@ -1,5 +0,0 @@
-//Kaldi Thrift 
-
-service KaldiService {
-  string kaldi_asr(1: string audio_file);
-}
