c46a270906caffff0d845b757eb1a62927810454
yunshengb <yba@umich.edu>
2016-05-16 13:35:31 -0400

    IMM: use GridFS so no size limit of the model

--- lucida/commandcenter/clear_db.sh
+++ lucida/commandcenter/clear_db.sh
@@ -1,7 +1,22 @@
 mongo
 use lucida
 # db.images_yba2.find()
+db.fs.files.find()
 db.images_yba2.count()
 db.images_yba2.drop()
 db.opencv_yba2.drop()
+db.fs.files.drop()
+exit
+
+
+
+
+mongo
+use lucida
+# db.images_Johann.find()
+db.fs.files.find()
+db.images_Johann.count()
+db.images_Johann.drop()
+db.opencv_Johann.drop()
+db.fs.files.drop()
 exit
