9650653c95b69bf8bc854072fee820d10d630d7c
Yunsheng Bai <yba@umich.edu>
2016-05-31 15:22:22 -0400

    Fix

---
+++ tools/query_mongo.cpp
@@ -0,0 +1,29 @@
+// g++ query_mongo.cpp -std=c++11 -lmongoclient -lboost_thread -lboost_filesystem -lboost_regex -lboost_program_options -lboost_system -pthread -lssl -lcrypto -o query_mongo
+
+#include <iostream>
+#include "mongo/client/dbclient.h"
+
+using namespace mongo;
+using namespace std;
+
+int main() {
+	DBClientConnection conn;
+	try {
+		conn.connect("localhost:27017");
+		cout << "Connected ok" << endl;
+	} catch( DBException &e ) {
+		cout << "Caught " << e.what() << endl;
+	}
+	string mongodbCollection = "lucida.images_Johann";  
+	auto_ptr<mongo::DBClientCursor> cursor = conn.query( mongodbCollection , mongo::BSONObj() );  
+	int count = 0;  
+	while (cursor->more()) {
+		cout << "@@@" << endl;
+		BSONObj p = cursor->next();
+		cout << p.getStringField("label") << endl;
+		++count;
+	}
+	cout << "### Numer of documents in lucida.images_Johann: " << count << endl;
+	return 0;
+}
+
