19bbebf1132dd67df6f2171b68ad214acf5a0086
Yunsheng Bai <yba@umich.edu>
2016-06-07 10:03:16 -0400

    CMD: coding style

--- lucida/commandcenter/controllers/LoginForm.py
+++ lucida/commandcenter/controllers/LoginForm.py
@@ -4,6 +4,5 @@ from wtforms import Form, TextField, PasswordField
 class LoginForm(Form):
 	# Fields of the form.
 	username = TextField('Username')
-	
 	password = PasswordField('Password')
 
--- lucida/commandcenter/controllers/ThriftServer.py
+++ lucida/commandcenter/controllers/ThriftServer.py
@@ -9,25 +9,28 @@ from ConcurrencyManagement import log
 from ThriftClient import thrift_client
 
 class LucidaServiceHandler(LucidaService.Iface):
- 
-    def __init__(self):
-        self.log = {}
- 
-    def create(self, LUCID, spec):
-        try:
-            log('Create ' + spec.content[0].type
-                 + ' at host ' + spec.content[0].data[0]
-                 + ' port ' + spec.content[0].tags[0])
-            thrift_client.add_service(spec.content[0].type,
-                                     spec.content[0].data[0],
-                                    spec.content[0].tags[0])
-        except Exception as e:
-            log(e)
-        return
- 
-    def learn(self, LUCID, knowledge):
-        log('Only create should be invoked')
-        return
-     
-    def infer(self, LUCID, query): 
-        return 'Only create should be invoked'
+	# Constructor.
+	def __init__(self):
+		self.log = {}
+
+	# Handles create: back-end service wants to register itself.
+	def create(self, LUCID, spec):
+		try:
+			log('Create ' + spec.content[0].type
+				 + ' at host ' + spec.content[0].data[0]
+				 + ' port ' + spec.content[0].tags[0])
+			thrift_client.add_service(spec.content[0].type,
+									 spec.content[0].data[0],
+									spec.content[0].tags[0])
+		except Exception as e:
+			log(e)
+		return
+
+	# Handles learn: do nothing.
+	def learn(self, LUCID, knowledge):
+		log('Only create should be invoked by back-end service')
+		return
+
+	# Handles infer: do nothing. 
+	def infer(self, LUCID, query): 
+		return 'Only create should be invoked by back-end service'
