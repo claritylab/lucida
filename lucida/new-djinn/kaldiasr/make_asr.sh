354b3c2c3dd2bf7821ba361e6d4a2b02e2cd26c9
Xiaowei Wang <xiaoweiw@umich.edu>
2016-06-14 01:18:06 -0400

    [Tonic] Fix asr Dockerfile

--- lucida/new-djinn/kaldiasr/make_asr.sh
+++ lucida/new-djinn/kaldiasr/make_asr.sh
@@ -1,8 +1,11 @@
 cd tools
 ./install_openfst.sh
+./atlas_header.sh
 cd ../src
 ./configure
 make depend -j $THREADS
 make -j $THREADS
 cd ..
+python -mthrift_compiler.main --gen cpp2 ../lucidaservice.thrift 
+python -mthrift_compiler.main --gen cpp2 ../lucidatypes.thrift 
 make all
