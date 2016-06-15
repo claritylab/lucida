64166b278713017c53864343cdea39a7d7ff7a80
yunshengb <yba@umich.edu>
2016-06-15 15:15:04 -0400

    CA: not talking to CMD

--- lucida/calendar/src/main/java/calendar/CalendarDaemon.java
+++ lucida/calendar/src/main/java/calendar/CalendarDaemon.java
@@ -29,36 +29,8 @@ import thrift.*;
  * Starts the calendar server and listens for requests.
  */
 public class CalendarDaemon {
-	private static void connectToCMD() {
-		QueryInput query_input = new QueryInput();
-		query_input.type = "CA";
-		query_input.data = new ArrayList<String>();
-		query_input.data.add("localhost");
-		query_input.tags = new ArrayList<String>();
-		query_input.tags.add("8084");
-		QuerySpec spec = new QuerySpec();
-		spec.content = new ArrayList<QueryInput>();
-		spec.content.add(query_input);
-		// Initialize thrift objects.
-		TTransport transport = new TSocket("localhost", 8080);
-		TProtocol protocol = new TBinaryProtocol(new TFramedTransport(transport));
-		LucidaService.Client client = new LucidaService.Client(protocol);
-		try {
-			transport.open();
-			System.out.println("Connecting to CMD at port " + 8080);
-			// Register itself to CMD.
-			client.create("", spec);
-			transport.close();
-			System.out.println("Successfully connected to CMD");
-		} catch (TException x) {
-			x.printStackTrace();
-		}
-	}
-	
 	public static void main(String [] args) 
 			throws TTransportException, IOException, InterruptedException {	
-		connectToCMD();
-		
 		TProcessor proc = new LucidaService.AsyncProcessor(
 				new CAServiceHandler.AsyncCAServiceHandler());
 		TNonblockingServerTransport transport = new TNonblockingServerSocket(8084);
