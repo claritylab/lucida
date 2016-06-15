ce39de0080ea1cf238a75903df11dc55e4f6fc7d
yunshengb <yba@umich.edu>
2016-06-15 15:16:58 -0400

    IMM: not talking to CMD

--- lucida/imagematching/opencv_imm/server/IMMServer.cpp
+++ lucida/imagematching/opencv_imm/server/IMMServer.cpp
@@ -32,33 +32,8 @@ using std::shared_ptr;
 using std::unique_ptr;
 using std::to_string;
 
-void registerToCMD() {
-	EventBase event_base;
-	shared_ptr<TAsyncSocket> socket(
-			TAsyncSocket::newSocket(&event_base, FLAGS_CMD_hostname,
-					FLAGS_CMD_port));
-	unique_ptr<HeaderClientChannel, DelayedDestruction::Destructor> channel(
-			new HeaderClientChannel(socket));
-	channel->setClientType(THRIFT_FRAMED_DEPRECATED);
-	LucidaServiceAsyncClient client(std::move(channel));
-	QueryInput query_input;
-	query_input.set_type("IMM");
-	query_input.data.push_back("localhost");
-	query_input.tags.push_back(to_string(FLAGS_port));
-	QuerySpec q;
-	q.content.push_back(query_input);
-	auto result = client.future_create("XXX", q).then(
-			[](folly::Try<folly::Unit>&& t) mutable {
-		cout << "Successfully registered to command center" << endl;
-	});
-
-	event_base.loop();
-}
 
 int main(int argc, char* argv[]) {
-	registerToCMD();
-
-	cout << "IMM at 8082" << endl;
 	google::InitGoogleLogging(argv[0]);
 	google::ParseCommandLineFlags(&argc, &argv, true);
 
@@ -70,6 +45,8 @@ int main(int argc, char* argv[]) {
 	server->setInterface(std::move(handler));
 	server->setIdleTimeout(std::chrono::milliseconds(0));
 	server->setTaskExpireTime(std::chrono::milliseconds(0));
+
+	cout << "IMM at 8082" << endl;
 
 	server->serve();
 
