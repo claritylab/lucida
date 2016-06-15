511e6337d9f23be9750ff058a7678af8e322b3ad
root <root@clarity13.eecs.umich.edu>
2016-06-15 15:02:40 -0400

    CMD: Add HTTPS

---
+++ lucida/commandcenter/apache/install_apache.sh
@@ -0,0 +1,13 @@
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+sudo apt-get update
+sudo apt-get -y upgrade
+sudo apt-get install -y \
+        apache2 \
+        apache2-utils \
+        libapache2-mod-wsgi \
+        python-pip
+cp -f conf/000-default.conf /etc/apache2/sites-available/000-default.conf
+sudo a2enmod ssl
+service apache2 restart
+
+
