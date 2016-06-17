98c2489baf241aaf8184decf7fe5d2484fcb9ffa
yunshengb <yba@umich.edu>
2016-06-16 23:08:59 -0400

    IMM: restore from Xiaowei

--- lucida/imagematching/opencv_imm/test/IMMClient.cpp
+++ lucida/imagematching/opencv_imm/test/IMMClient.cpp
@@ -106,7 +106,6 @@ int main(int argc, char* argv[]) {
 			std::unique_ptr<HeaderClientChannel, DelayedDestruction::Destructor>(
 					new HeaderClientChannel(socket_t)));
 	
-  /*
   // Open the images.
 	string db = fs::current_path().string() + "/test_db";
 	fs::path p = fs::system_complete(db);
@@ -134,7 +133,7 @@ int main(int argc, char* argv[]) {
 		cout << "Going to loop" << endl;
 		event_base.loop();
 	}
-  */
+  
 
 	// Infer.
 	// Make request.
