3ef9a37cc13b9cc0a9e6e8bffae4b3c2c9102984
yunshengb <yba@umich.edu>
2016-07-01 22:30:44 -0400

    CMD: fix

--- lucida/commandcenter/controllers/Create.py
+++ lucida/commandcenter/controllers/Create.py
@@ -9,6 +9,9 @@ create = Blueprint('create', __name__, template_folder='templates')
 @login_required
 def create_route():
 	# Display.
-	
-	return render_template("create.html")
-
+	services_list = []
+	for service_name in thrift_client.SERVICES:
+		host, port = thrift_client.get_service(service_name)
+		services_list.append((service_name, host, port))
+	return render_template("create.html", service_list=
+		sorted(services_list, key=lambda i: i[0]))
