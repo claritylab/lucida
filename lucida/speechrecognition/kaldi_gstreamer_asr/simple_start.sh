2ec07e9725adf07fd688e8a4369738c5cce5c3f2
yunshengb <yba@umich.edu>
2016-07-09 17:45:45 -0400

    ASR: fix port

--- lucida/speechrecognition/kaldi_gstreamer_asr/simple_start.sh
+++ lucida/speechrecognition/kaldi_gstreamer_asr/simple_start.sh
@@ -1,5 +1,3 @@
-#gnome-terminal -x bash -c "python kaldigstserver/master_server.py --port=8888; read -n1"
-
 export GST_PLUGIN_PATH=kaldi/tools/gst-kaldi-nnet2-online/src/:kaldi/src/gst-plugin/
 
-gnome-terminal -x bash -c "python kaldigstserver/worker.py -u ws://localhost:8888/worker/ws/speech -c sample_english_nnet2.yaml; read -n1"
+gnome-terminal -x bash -c "python kaldigstserver/worker.py -u ws://localhost:8081/worker/ws/speech -c sample_english_nnet2.yaml; read -n1"
