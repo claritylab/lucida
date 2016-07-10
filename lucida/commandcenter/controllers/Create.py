9ccacf20d2a301ff9e7081f4a7a425d29dc7114a
yunshengb <yba@umich.edu>
2016-07-10 18:44:28 -0400

    CMD: fixed learn

--- lucida/commandcenter/controllers/Create.py
+++ lucida/commandcenter/controllers/Create.py
@@ -9,23 +9,14 @@ create = Blueprint('create', __name__, template_folder='templates')
 @login_required
 def create_route():
 	options = {}
-	username = session['username']
 	try:
 		# Retrieve pre-configured services.
 		services_list = []
 		for service in thrift_client.SERVICES.values():
 			host, port = service.get_host_port()
 			services_list.append((service.name, host, port))
-		
-		# Deal with POST requests.
-		if request.method == 'POST':
-			# If the request does not contain an "op" field.
-			if not 'op' in request.form:
-				raise RuntimeError('Did you click the button?')
+		options['service_list']= sorted(services_list, key=lambda i: i[0])
 	except Exception as e:
 		log(e)
-		if str(e) == 'TSocket read 0 bytes':
-			e = 'Back-end service encountered a problem'
 		options['error'] = e			
-	return render_template("create.html", service_list=
-		sorted(services_list, key=lambda i: i[0]))
+	return render_template("create.html", **options)
