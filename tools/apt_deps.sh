1ae96f8febd10eaa7f1116a4c0195450f4fab87b
Yunsheng Bai <yba@umich.edu>
2016-08-29 17:32:45 -0400

    Fix apt_deps.sh

--- tools/apt_deps.sh
+++ tools/apt_deps.sh
@@ -71,3 +71,6 @@ apt-get install -y zlib1g-dev \
                    python-gobject-2 \
                    vim \
                    memcached
+pip install --upgrade distribute
+pip install --upgrade pip
+pip install -r python_requirements.txt
