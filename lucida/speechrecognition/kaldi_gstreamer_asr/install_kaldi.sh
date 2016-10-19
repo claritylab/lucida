4f33edc92bd347d8b883846c38222afe797ec910
Yunsheng Bai <yba@umich.edu>
2016-10-18 20:13:11 -0400

    Add kaldi gstreamer plugin

--- lucida/speechrecognition/kaldi_gstreamer_asr/install_kaldi.sh
+++ lucida/speechrecognition/kaldi_gstreamer_asr/install_kaldi.sh
@@ -18,6 +18,7 @@ make ext
 cd gst-plugin
 sudo apt-get install -y libgstreamer1.0-dev
 sudo apt-get install -y gstreamer1.0-plugins-good
+sudo apt-get install -y gstreamer1.0-plugins-bad
 sudo apt-get install -y gstreamer1.0-plugins-ugly
 sudo apt-get install -y gstreamer1.0-tools
 make depend
