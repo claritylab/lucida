bf9d6c9ebd98e9acd1f015b83eff2ada99af6016
Saijel Mokashi <saijel@clinc.com>
2017-04-30 23:58:27 +0000

    allowing temporary access to weather api keys

--- lucida/weather/WeatherConfig.py
+++ lucida/weather/WeatherConfig.py
@@ -12,5 +12,4 @@ WU_API_KEY = 'ff76e8e80c802d46' # TODO: remove after PR has been accepted # add 
 # Open Weather Map API key
 # https://openweathermap.org/api
 OWM_API_URL_BASE = 'http://api.openweathermap.org/data/2.5/weather?'
-#OWM_API_KEY = '362537b891e6df03a316e82565fe4df3' # TODO: remove after PR has been accepted # add your API key here
-OWM_API_KEY = 'hi362537b891e6df03a316e82565fe4df3' # TODO: remove after PR has been accepted # add your API key here
+OWM_API_KEY = '362537b891e6df03a316e82565fe4df3' # TODO: remove after PR has been accepted # add your API key here
