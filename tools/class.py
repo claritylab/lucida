7e9d7467e0d017a5396cec63dc01c04d752be9f1
zhexuan <zhexuanc@umich.edu>
2017-07-13 11:51:16 -0400

    clean stuff

--- tools/class.py
+++
@@ -1,6 +0,0 @@
-class MSWF(workFlow): 
-	def processCurrentState(self,inputModifierText,inputModifierImage): 
-		if(self.currentState==0):
-			self.batchedData = [serviceRequestData("MS",inputModifierText[0])] 
-			self.isEnd = True
-			return
